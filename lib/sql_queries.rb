ea68e83d612ad72de74d93604b097b6c0ec8c659
Veronica Kim <vero.a.kim@gmail.com>
2018-04-26 12:43:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT gender FROM bears ORDER BY name, age;"
+  "SELECT female FROM bears ORDER BY name, age;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
