558cddfbc3c734efbca8204f89503fe712a184c9
Veronica Kim <vero.a.kim@gmail.com>
2018-04-26 12:58:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,6 @@
-# def selects_all_female_bears_return_name_and_age
-#   "SELECT name, age FROM bears WHERE gender="F";"
-# end
+def selects_all_female_bears_return_name_and_age
+  "SELECT name, age FROM bears WHERE gender="F";"
+end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name ASC;"
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE MAX(age);"
+  "SELECT MAX(age) FROM bears "
 end
 
 def select_youngest_bear_and_returns_name_and_age
