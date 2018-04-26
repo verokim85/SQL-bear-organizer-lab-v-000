574142e1f1765b0367b646b5d872c9c92bb6a791
Veronica Kim <vero.a.kim@gmail.com>
2018-04-26 12:48:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT female FROM bears ORDER BY name, age;"
+  "SELECT name, age FROM bears WHERE breed = f;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT MAX(age) FROM bears "
 end
 
 def select_youngest_bear_and_returns_name_and_age
