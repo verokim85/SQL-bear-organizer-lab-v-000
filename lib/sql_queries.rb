d0687ab9b860d774ac20755dada687b6a06bd66e
Veronica Kim <vero.a.kim@gmail.com>
2018-04-26 13:14:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM cats WHERE color = 'dark brown';"
+  "SELECT color, COUNT(breed) FROM cats;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -29,4 +29,3 @@ end
 def selects_bear_that_killed_Tim
   "Write your SQL query here"
 end
- 
